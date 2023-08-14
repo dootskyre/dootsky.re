e85f1bd0f3d988f721ed3e0d11d85162c5d5adee
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-08-14 15:57:59 +0800

    Adjust navbar and dot colors

--- _javascript/main.js
+++ _javascript/main.js
@@ -16,37 +16,31 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 $(document).ready(function() {
-  $(".slider").slick({
-      centerMode: true,
+  $(".custom-slider").slick({
       centerPadding: "60px",
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 3000,
-      infinite: true,
       variableWidth: true,
+      infinite: false,
+      draggable: true,
       dots: true,
+      prevArrow: $(".slick-prev"),
+      nextArrow: $(".slick-next"),
       responsive: [
           {
               breakpoint: 1024,
               settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-                  infinite: true,
+                  slidesToShow: 2, // Show 2 slides on medium screens
+                  slidesToScroll: 2,
                   dots: true
               }
           },
           {
               breakpoint: 600,
               settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2
-              }
-          },
-          {
-              breakpoint: 480,
-              settings: {
-                  slidesToShow: 1,
+                  slidesToShow: 1, // Show 1 slide on small screens
                   slidesToScroll: 1
               }
           }
