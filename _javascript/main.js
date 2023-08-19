6ae47aaf96122916772da9d1d6c1eb0c6a9fd2b1
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-08-19 19:08:25 +0800

    Move slick JS into HTML

--- _javascript/main.js
+++ _javascript/main.js
@@ -14,36 +14,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
-
-$(document).ready(function() {
-  $(".slider").slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      variableWidth: true,
-      infinite: true,
-      draggable: true,
-      dots: true,
-      centerMode: true,
-      prevArrow: $(".carousel-previous"),
-      nextArrow: $(".carousel-next"),
-      responsive: [
-          {
-              breakpoint: 1024,
-              settings: {
-                  slidesToShow: 2, // Show 2 slides on medium screens
-                  slidesToScroll: 2,
-                  dots: true
-              }
-          },
-          {
-              breakpoint: 600,
-              settings: {
-                  slidesToShow: 1, // Show 1 slide on small screens
-                  slidesToScroll: 1
-              }
-          }
-      ]
-  });
-});
